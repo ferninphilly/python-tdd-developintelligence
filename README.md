78b7286f90c1a78390fb857e446c89715c2d694e
Fernando Pombeiro <fernpombeiro@yahoo.com>
2019-12-18 20:09:38 +0000

    Updated presentation

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Python Test Driven Development
-This is the github repo for the Python TDD and Deployment course for Salesforce
+This is the github repo for the Python TDD and Deployment course for Salesforce.
+This course will cover the following topics:
+
+* Test Driven Development
+* Continuous Improvement
+* Continuous Development
+* Building and maintaining coding pipelines in terraform
+* 
+
+# Pre-requisites
+
+Before we start we will need the class to have the following items downloaded to their systems. 
+
 
 
--- module_02/lab_02/databasemock/databasemock.py
+++ module_02/lab_02/databasemock/databasemock.py
@@ -1,5 +1,6 @@
 import yaml
 import pymysql
+import csv
 
 def get_config(config_file):
   with open(config_file) as f:
@@ -10,7 +11,15 @@ def connect_to_db(cfg):
   conn = pymysql.connect(host=cfg['host'], port=cfg['port'], user=cfg['user'], passwd=cfg['user'], db=cfg['db'])
   return conn.cursor
 
-def read_from_file(filename):
-  with open(sample_file, 'r') as f:
-    f.read()
+def read_from_file(filename, cursor):
+  with open(sample_file, 'r') as csvfile:
+    csv_data = csv.reader(csvfile, delimiter=",")
+    for row in csv_data:
+      if row:
+        cursor.execute("""
+        INSERT INTO myawesomedb 
+        (user_id,first_name,last_name,adress,phone_number,psychlo,man_animal,likes_rats,Qualifications")
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, row)
+    
 
